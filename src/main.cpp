31a81a782862b1403bd28ffa325ecfc2f429417f
Michael Nguyen <michael.nguyen.nhu@gmail.com>
2025-12-08 22:46:58 -0500

    Finished Skeleton: Added base files for src, include, and tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,18 @@
 #include <cstddef>
 #include <iostream> 
 
-
+/*
+#include "event.h"
+#include "ring_buffer.h"
+#include "latency_tracker.h"
+#include "event_bus.h" 
+*/
 
 int main() {
     std::cout << "Low Latency Event Bus Benchmark Stub" << std::endl; 
 
     std::size_t capacity = 1024; //temporary, will implement power of two 
+
+    std::cout << "Environment Set up. Capacity = " << capacity  << std::endl; 
+    return 0; 
 }
